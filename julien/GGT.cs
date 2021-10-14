efc47b48d4546802f648cbadd6a2c81a677501fa
Julian Petersen <juli11c0@edu.ucl.dk>
2021-10-14 14:03:34 +0200

    Add project files.

---
+++ julien/GGT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace julien
+{
+    class GGT
+    {
+        public int GGTFunction(int a, int b)
+        {
+            if (a - b == 0)
+                return a;
+            else
+            {
+                int ggt = a - b;
+
+                if (b < ggt)
+                {
+                    b = a;
+                    a = ggt;
+                }
+                a = b;
+                b = ggt;
+
+                return GGTFunction(a, b);
+            }
+        }
+    }
+}
---
+++ julien/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace julien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 28, b = 16;
+            //int ggt = a - b;
+
+            GGT ggt = new GGT();
+
+            int result = ggt.GGTFunction(a, b);
+
+            Console.WriteLine(result);
+
+            //bool stopcondition = false;
+
+            //while (!stopcondition)
+            //{
+            //    if (a - b == 0)
+            //    {
+            //        Console.WriteLine($"GGT is {a}");
+            //        stopcondition = true;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"a is {a}");
+            //        Console.WriteLine($"b is {b}");
+
+            //        int ggt = a - b;
+
+            //        if (b < ggt)
+            //        {
+            //            b = a;
+            //            a = ggt;
+            //        }
+
+            //        a = b;
+            //        b = ggt;
+            //        Console.WriteLine($"ggt is {ggt}");
+            //    }
+            //}
+        }
+
+    }
+}
